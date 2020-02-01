efbcfcd1367a3ca0565e502e2ea3ff2a0cf25667
Jared S <jaredjames2020@gmail.com>
2020-02-01 19:04:30 +0000

    Done.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -6,10 +6,12 @@ describe Student do
   end
 
   it 'can be created' do
+    student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
     expect(@student).to be_valid
   end
 
   it 'has a to_s instance method' do
     expect(@student.to_s).to eq("Daenerys Targaryen")
   end
+
 end
